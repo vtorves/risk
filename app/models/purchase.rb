b3016fd8bebbf854c9cbcdef56584995eae7187e
Vinicius Torves <vinicius@torves.com>
2023-10-28 17:57:56 -0400

    improve validations on purchase model

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,12 +1,31 @@
+# == Schema Information
+#
+# Table name: purchases
+#
+#  id                 :uuid             not null, primary key
+#  transaction_id     :integer          not null
+#  merchant_id        :integer          not null
+#  user_id            :integer          not null
+#  card_number        :string           not null
+#  transaction_date   :datetime         not null
+#  transaction_amount :decimal(10, 2)   not null
+#  device_id          :integer
+#  has_cbk            :boolean          default(FALSE)
+#  status_updated_at  :datetime
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  status             :integer          default("none")
+#
 class Purchase < ApplicationRecord
   enum :status, { none: 0, approve: 1, deny: 2 }, suffix: true
 
-  validates :transaction_id, presence: true#, uniqueness: true
+  validates :transaction_id, presence: true, uniqueness: true
   validates :merchant_id, presence: true, numericality: { only_integer: true }
   validates :user_id, presence: true, numericality: { only_integer: true }
 
   validates :card_number, presence: true,
-                          format: { with: /\A\d{6}\*+\d{4}\z/, message: "must match the format '######******####'" }
+    format: { with: /\A\d{6}\*+\d{4}\z|\A\d{6}\*+\d{5}\z/,
+      message: "must match the format '######******####' with either 15 or 16 digits" }
 
   validates :transaction_date, presence: true
   validates :transaction_amount, presence: true,
