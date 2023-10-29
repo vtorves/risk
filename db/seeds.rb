9a2dca6611894deb7874d6bca49c479aa5d98f33
Vinicius Torves <vinicius@torves.com>
2023-10-28 21:28:32 -0400

    import data from csv to purchase model development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+if ENV['RAILS_ENV'] == 'development'
+
+  require 'csv'
+
+  csv_file = Rails.root.join('db', 'purchase.csv')
+
+  CSV.foreach(csv_file, headers: true) do |row|
+    Purchase.create!(
+      transaction_id: row['transaction_id'],
+      merchant_id: row['merchant_id'],
+      user_id: row['user_id'],
+      card_number: row['card_number'],
+      transaction_date: row['transaction_date'],
+      transaction_amount: row['transaction_amount'],
+      device_id: row['device_id'],
+      has_cbk: row['has_cbk'].downcase == 'true'
+    )
+  end
+  puts "CSV imported successfully."
+end
