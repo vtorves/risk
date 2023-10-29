a8ee046953b459867620bc507d14a5a2f33cf325
Vinicius Torves <vinicius@torves.com>
2023-10-28 21:23:57 -0400

    add route to purchase

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+ namespace :api, defaults: { format: :json } do
   # Defines the root path route ("/")
   # root "articles#index"
+    namespace :v1 do
+      resources :purchases, only: [:create]
+    end
+  end
 end
